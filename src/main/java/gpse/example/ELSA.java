22e38f5b9f626160e795e8901081fdba49d72c19
Maiko Möbius <mmoebius@techfak.uni-bielefeld.de>
2021-04-14 23:13:42 +0200

    expanded the commandline query to create envelops. Navigator: Onur Torman. Closes #29472 @0:20h

--- src/main/java/gpse/example/ELSA.java
+++ src/main/java/gpse/example/ELSA.java
@@ -1,6 +1,7 @@
 package gpse.example;
 
 import gpse.example.domain.Document;
+import gpse.example.domain.Envelop;
 import org.apache.tomcat.util.net.jsse.JSSEUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.File;
 import java.io.IOException;
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
@@ -17,7 +21,7 @@ public class ELSA {
     /**
      * The main method which evaluates the  inputs on the commandline.
      * Commands:
-     * end
+     * exit
      * import <path>
      * sign <?>
      * help
@@ -34,26 +38,38 @@ public class ELSA {
             String[] input = line.split(" ");
             switch (input[0]) {
                 case "help":
-                    System.out.println("end               -ends the programm");
+                    System.out.println("exit              -terminates the programm");
                     System.out.println("import <path>     -imports a document or an envelop");
                     System.out.println("sign <?>          -signs a document");
                     System.out.println("server            -starts the server");
                     break;
-                case "end":
+                case "exit":
                     System.exit(0);
                     break;
                 case "import":
                     if (input.length > 1) {
                         File file = new File(input[1]);
-                        if (file.exists() && file.isFile()) {
+                        if (file.exists() && file.isFile() && input.length == 2) {
                             try {
                                 document = new Document(input[1]);
                                 System.out.println("import of " + document.getDocumentMetaData().getMetaDocumentTitle() + " successful.");
                             } catch (IOException e) {
                                 System.out.println("invalid path");
                             }
-                        } else if (file.exists() && file.isDirectory()) {
-                            //Enevelop
+                        } else if (file.exists() && (file.isDirectory() || input.length > 2)) {
+                            Envelop envelop = null;
+                            try {
+                                if (input.length > 2) {
+                                    System.out.println("Type in the name of the envelop: ");
+                                    String name = scanner.nextLine();
+                                    envelop = new Envelop(name, Arrays.asList(input).subList(1, input.length));
+                                } else {
+                                    envelop = new Envelop(input[1]);
+                                }
+                            } catch (IOException e) {
+                                System.out.println("Some paths were invalid.");
+                            }
+                            System.out.println(envelop.getDocumentList());
                         } else {
                             System.out.println("document not found");
                         }
