0bde59ddc37a9e65b02b18b90ff6a1c4368f2db8
Hannah Schweizer <hschweizer@techfak.uni-bielefeld.de>
2021-05-04 16:55:56 +0200

    fixes #29790: removed background-images from 404 and no-connection page

--- src/main/vue/views/NotFoundPage.vue
+++ src/main/vue/views/NotFoundPage.vue
@@ -1,44 +1,43 @@
 <template>
-    <b-overlay id="background">
-        <b-container fluid id="container">
-            <b-row cols="1">
-                <b-col>
-                    <Header></Header>
-                </b-col>
-            </b-row>
+    <b-container fluid id="container">
+        <b-row cols="1">
+            <b-col>
+                <Header></Header>
+            </b-col>
+        </b-row>
 
-            <b-row class="mb-2 mt-5 text-center" align-v="center" align-h="center" cols="1">
-                <b-col id="h1" class="text">
-                    <h1>{{ $t('NotFoundPage.h1') }}</h1>
-                </b-col>
-                <b-col class="text">
-                    <h3>{{ $t('NotFoundPage.h3') }}</h3>
-                </b-col>
-                <b-col class="text">
-                    <h6>{{ $t('NotFoundPage.errorCode') }}</h6>
-                </b-col>
-            </b-row>
+        <b-row class="mb-2 mt-5 text-center" align-v="center" align-h="center" cols="1">
+            <b-col id="h1" class="text">
+                <h1>{{ $t('NotFoundPage.h1') }}</h1>
+            </b-col>
+            <b-col class="text">
+                <h3>{{ $t('NotFoundPage.h3') }}</h3>
+            </b-col>
+            <b-col class="text">
+                <h6>{{ $t('NotFoundPage.errorCode') }}</h6>
+            </b-col>
+        </b-row>
 
-            <b-row class="mt-5 text-center" align-v="center" align-h="center" cols="2">
-                <b-col cols="3" class="text">
-                    <div class="link" @click="historyThere()  ? $router.go(-1) : $router.push('/')">
-                        {{ $t('NotFoundPage.back') }}
-                    </div>
-                </b-col>
-                <b-col cols="3" class="text">
-                    <div class="link" @click="$router.push(`/${$i18n.locale}/test-page`)">
-                        {{ $t('NotFoundPage.start') }}
-                    </div>
-                </b-col>
-            </b-row>
+        <b-row class="mt-5 text-center" align-v="center" align-h="center" cols="2">
+            <b-col cols="3" class="text">
+                <div class="link" @click="historyThere()  ? $router.go(-1) : $router.push('/')">
+                    {{ $t('NotFoundPage.back') }}
+                </div>
+            </b-col>
+            <b-col cols="3" class="text">
+                <!-- To-Do: Add  real Route to Home -->
+                <div class="link" @click="$router.push(`/`)">
+                    {{ $t('NotFoundPage.start') }}
+                </div>
+            </b-col>
+        </b-row>
 
-            <b-row cols="1">
-                <b-col>
-                    <Footer></Footer>
-                </b-col>
-            </b-row>
-        </b-container>
-    </b-overlay>
+        <b-row cols="1">
+            <b-col>
+                <Footer></Footer>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
@@ -70,8 +69,4 @@ export default {
     margin-top: 7vw;
 }
 
-#background {
-    background-image: url(../assets/background.png);
-    background-size: cover;
-}
 </style>
