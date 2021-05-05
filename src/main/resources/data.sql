38c86566476701d6f869d34c40c2eaab9ef478d6
tkroecker <tkroecker@techfak.uni-bielefeld.de>
2021-05-05 16:59:29 +0200

    refs #29725 @0.1h: Added example data via data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (personalData, email, firstname, lastname, publicKey, password, admin) VALUES (null, 'hans.schneider@mail.com', 'hans', 'schneider', null, '12345678', false);
+INSERT INTO user (personalData, email, firstname, lastname, publicKey, password, admin) VALUES (null, 'peter.müller@mail.com', 'peter', 'müller', null, '87654321', true)
