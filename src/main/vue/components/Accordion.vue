be69b9cb607cb3eddb220a8a13e12c304dc0c239
Onur Toraman <otoraman@techfak.uni-bielefeld.de>
2021-05-09 11:49:17 +0200

    Refactored the UserGuide Page and Accordion Component: new added points will show a dropdwown with its answer. Also colored the arrow icon in the Elsa logo color and added some css changes for mobile devices, refs 29917 @5.5h

---
+++ src/main/vue/components/Accordion.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <div :class="open ? '{{generalInfo}} open' : '{{generalInfo}}'">
+        <BIcon></BIcon>
+
+        <div class="question" @click="$emit('toggleOpen', index)">
+            <BIconCaretRightFill class="ui-icon-caret-1-n"></BIconCaretRightFill>
+
+            <h2 class="h2-head">{{ generalInfo.question }} </h2>
+        </div>
+        <div class="answer">{{generalInfo.answer}}</div>
+
+        <h2 class="header-section-2" style="margin-left: 3em;
+            font-size: 1.6em;
+            margin-top: 1.2em;
+            color: #436495"
+            v-if="index === 2">
+            Rund ums Signieren
+        </h2>
+        <h2 class="header-section-2" style="margin-left: 3em;
+            font-size: 1.6em;
+            margin-top: 1.2em;
+            color: #436495"
+            v-if="index === 8">
+            Rund ums Signieren
+        </h2>
+    </div>
+</template>
+
+<script>
+import { BIcon, BIconCaretRightFill } from 'bootstrap-vue'
+export default {
+    name: 'Accordion',
+    props: ['generalInfo', 'index', 'open', 'section'],
+    components: {
+        BIcon,
+        BIconCaretRightFill
+    }
+}
+</script>
--- src/main/vue/views/UserGuide.vue
+++ src/main/vue/views/UserGuide.vue
@@ -1,135 +1,271 @@
 <template>
-    <div>
-        <Header></Header>
-        <b-container>
-            <BaseHeading name=""></BaseHeading>
-            <h2> {{ $t('UserGuide.name') }} </h2>
-            <b-container id="general-information">
-                <b-row>
-                    <h4 style="font-size: 26px; margin-left: 18px; margin-top: 5px; margin-bottom: -20px; color: #436495"> Allgemeine Information</h4>
-                </b-row>
-                <b-row>
-                    <div class="collapse-block">
-                        <input id="collapsible" class="arrow-toggle" type="checkbox">
-                        <label for="collapsible" class="label-collapsible-header"> Was ist Elsa? </label>
-                        <div class="collapsible-content">
-                            <div class="content-inner">
-                                <p style="margin-left: 20px">
-                                    QUnit is by calling one of the object that are embedded in JavaScript
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </b-row>
-                <b-row>
-                    <p style="margin-left: 20px; margin-top: -8px">Wie funktioniert Elsa?</p>
-                </b-row>
-            </b-container>
+    <div id="app">
+        <Header class="top-header"></Header>
+        <BaseHeading class="base-header" name=""></BaseHeading>
+        <h2 class="h2-user-guide"> Benutzerhandbuch </h2>
 
-        </b-container>
+        <div class="generalInfo">
+            <h2 class="h2-general-info">Allgemeine Information</h2>
+            <b-col class="col-for-mobile">
+                <Accordion
+                    v-for="(generalInfo, i) in generalInfos"
+                    :general-info="generalInfo"
+                    :index="i"
+                    :key="i"
+                    :open="generalInfo.open"
+                    @toggleOpen="toggleOpen"
+                />
+            </b-col>
+        </div>
 
     </div>
 </template>
 
 <script>
+import Accordion from "@/main/vue/components/Accordion";
 import Header from "@/main/vue/components/Header";
 export default {
     name: "UserGuide",
-    components: {Header}
+    components: {Header, Accordion},
+    data: function () {
+        return {
+            generalInfos: [
+
+                {
+                    open: false,
+                    question: 'My journey with Vue',
+                    answer: 'QUnit is by calling one of the object that are embedded in JavaScript\n'
+                },
+                {
+                    open: false,
+                    question: 'Blogging with Vue',
+                    answer: 'QUnit is by calling one of the object that are embedded in JavaScript\n'
+
+                },
+                {
+                    open: false,
+                    question: 'Why Vue is so fun',
+                    answer: 'QUnit is by calling one of the object that are embedded in JavaScript\n'
+
+                },
+                // Section for Signature
+                {
+                    open: false,
+                    question: "Wie unterschreibe ich ein Dokument?",
+                    answer: "halloooo"
+                },
+                {
+                    open: false,
+                    question: "Ist das digitale Unterschreiben überhaupt sicher?",
+                    answer: "Halloooo 2"
+                },
+                {
+                    open: false,
+                    question: "Wie funktioniert die kryptographische Signatur?",
+                    answer: "halloooo 3"
+                }
+            ],
+        }
+    },
+    methods: {
+        toggleOpen: function (index) {
+            this.generalInfos = this.generalInfos.map((generalInfo, i) => {
+                if (index === i) {
+                    generalInfo.open = !generalInfo.open;
+                } else {
+                    generalInfo.open = false;
+                }
+                return generalInfo;
+            });
+
+        }
+    }
 }
 </script>
 
-<style scoped>
-@import '/src/main/vue/assets/css/global.css';
+<style>
 
+.base-header {
+    margin-top: 2.2em;
+}
 
-h2 {
+.h2-user-guide {
     display: flex;
     align-items: center;
-    margin-top: 100px;
-    font-size: 40px;
+    margin-top: 3em;
+    font-size: 2em;
+    margin-left: 5rem;
+    text-align: left;
 }
 /* for the line after the headline */
-h2::after {
+.h2-user-guide::after {
     content: '';
     flex: 1;
     margin-left: .5rem;
-    height: 1px;
+    height: .1rem;
+    width: 100%;
     background-color: #000;
 }
-
-
-input[type='checkbox']
-    { display: none;
+.h2-general-info {
+    margin-left: 3.8em;
+    font-size: 1.6em;
+    margin-top: 1.2em;
+    color: #436495;
 }
-.collapse-block
-    {
-    margin: 1.2rem 1px 10px 2rem;
-    }
-.label-collapsible-header
-    {
-    display: block;
-    font-size: 19px;
+.generalInfo .header-section-2 {
+    font-size: 1.4em;
+}
+
+.generalInfo {
     text-align: left;
-    padding: .5rem;
-    margin-bottom: -4px;
-    color: black;
+}
+
+.generalInfo .question .h2-head {
+    font-size: .8em;
+    text-align: left;
+    margin-left: 10em;
+    margin-top: -20px;
+}
+
+
+body {
+    background-color: #EEE;
+    font-family: sans-serif;
+}
+
+.generalInfo {
+    width: 100%;
+    max-width: 768px;
     background: white;
+}
+.generalInfo .question {
+    alignment: left;
+    color: #3c3c3c;
+    font-size: 30px;
+    margin-top: -1em;
+    transition: all 0.4s linear;
+    margin-left: -1.3em;
     cursor: pointer;
-    border-radius: 5px;
-    transition: all 0.25s ease-out;
-    }
-.label-collapsible-header:hover
-    {
-    color: blue;
-    }
-.label-collapsible-header::before
-    {
-    content: ' ';
-    display: inline-block;
-    border-top: 5px solid transparent;
-    border-bottom: 5px solid transparent;
-    border-left: 5px solid currentColor;
-    vertical-align: middle;
-    margin-right: .7rem;
-    transform: translateY(-2px);
-    transition: transform .2s ease-out;
 }
 
+/* for screen width which are equal 500px or less*/
+@media only screen and (max-width: 500px) {
+    /* For the Right Arrow (Go Back arrow)*/
+    .base-header {
+        margin-top: 3em;
+    }
 
-/* rotating the arrow toogle */
-.arrow-toggle:checked+.label-collapsible-header::before
-{
-    transform: rotate(90deg) translateX(-3px);
+    .top-header {
+        height: 7%;
+    }
+
+    /* for user guide headers */
+    .h2-user-guide {
+        margin-top: 2.4em;
+        margin-left: 2em;
+        font-size: 1.8em;
+    }
+
+    .generalInfo {
+        margin-left: -1.4em;
+    }
+
+    /* for faq section (general infos) */
+    .h2-general-info {
+        font-size: 1.6em;
+        margin-top: 1.6em;
+    }
+    /* for faq headers */
+    .generalInfo .question{
+        font-size: 1.5em;
+        margin-left: -4em;
+        transition: top 4s ease 0s;
+
+    }
+    /* for faq answer of the headers */
+    .generalInfo .answer {
+        font-size: 1em;
+        margin-left: 10em;
+        overflow-y: hidden;
+        text-align: left;
+        opacity: 0;
+        max-height: 0;
+        transition: top 4s ease 0s;
+    }
+    /* for faq answer for the open cause (collapsed) */
+    .generalInfo .open .answer {
+        opacity: 1;
+        font-size: .9em;
+        max-height: 1000px;
+        margin-left: 7em;
+        transform: translateY(-10%);
+
+    }
+    /* for the icon in the faq header */
+    .generalInfo .question .ui-icon-caret-1-n {
+        content: '';
+        margin-left: 7em;
+        transform: translateY(0%) rotate(0deg);
+        width: 10px;
+        height: 10px;
+        color: #436495;
+    }
+    /* for the icon in the faq header open cause (collapsed) */
+    .generalInfo .open .question .ui-icon-caret-1-n {
+        transform: translateY(5%) rotate(90deg);
+        color: #436495;
+        width: 10px;
+        height: 10px;
+    }
+    /* for the faq section (signaure) */
+    .generalInfo .header-section-2 {
+        font-size: 10em;
+    }
 }
-.arrow-toggle:checked + .label-collapsible-header + .collapsible-content
-    {
-    max-height: 100px;
-    }
-.arrow-toggle:checked+.label-collapsible-header
-    {
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-    }
 
-/* collapsible innter backgorund,border*/
-.collapsible-content .content-inner
-    {
-    background: white;
+.generalInfo .open .question {
+    margin-bottom: 0px;
+}
+/* for screen width of 550px or greater than 550px */
+@media screen and (min-width: 550px ) {
+    .top-header {
+        height: 10%;
     }
+    .generalInfo .col-for-mobile {
+        margin-left: -3em;
+    }
+    .generalInfo .header-section-2 {
+        padding-left: 2em;
+    }
+    .generalInfo .question {
+        font-size: 1.5em;
+    }
+    .generalInfo .question .ui-icon-caret-1-n {
+        content: '';
+        margin-left: 7em;
+        transform: translateY(30%) rotate(0deg);
+        width: 15px;
+        height: 15px;
+        color: #436495;
+    }
+    .generalInfo .open .answer {
+        margin-left: 11.4em;
+        font-size: .9em;
+    }
+}
 
-/* for the content div
-transition for the time of the collapsing */
-.collapsible-content
-{
+.generalInfo .answer {
+    color: #3c3c3c;
+    font-size: 18px;
+    margin-left: 15.5rem;
+    text-align: left;
+    opacity: 0;
     max-height: 0;
-    margin-left: .5em;
-    overflow: hidden;
-    transition: max-height .30s ease-in-out;
+    overflow-y: hidden;
+    /*transition: all 0.5s ease-in-out;*/
 }
-/* Here is the Style settings for the content paragraph(p) of the collapsible*/
-.collapsible-content p
-    {
-    font-size: 12px;
-    }
+.generalInfo .open .answer {
+    opacity: 1;
+    max-height: 1000px;
+}
 </style>
