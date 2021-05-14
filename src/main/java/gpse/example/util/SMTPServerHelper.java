9779062ca10735b33c6a24cb972530b71f456b7a
Jonas <jonashaschke@icloud.com>
2021-05-14 15:30:00 +0200

    Checkstyle in SMTPServerHelper, Protocol and PDFWriter corrected.

--- src/main/java/gpse/example/util/SMTPServerHelper.java
+++ src/main/java/gpse/example/util/SMTPServerHelper.java
@@ -11,15 +11,6 @@ import java.util.Properties;
 
 public class SMTPServerHelper {
 
-    private static final String TRUE = "true";
-    private static JavaMailSenderImpl mailSender;
-
-    private static String hostServer;
-
-    private static int port;
-
-    private static String userName;
-
     /**
      * Template for sending RegisterValidationEmail.
      */
@@ -32,6 +23,16 @@ public class SMTPServerHelper {
      * The subject of Elsas emails.
      */
     public static final String REGISTRATION_SUBJECT = "ELSA Registrierung";
+
+    private static final String TRUE = "true";
+
+    private static JavaMailSenderImpl mailSender;
+
+    private static String hostServer;
+
+    private static int port;
+
+    private static String userName;
 
     private static String password;
 
@@ -80,7 +81,7 @@ public class SMTPServerHelper {
      * @param link validation link.
      */
 
-    public static void sendRegistrationEmail(String toAddress, String userName, String link) {
+    public static void sendRegistrationEmail(final String toAddress, final String userName, final String link) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("noreply@gmail.com");
         message.setTo(toAddress);
