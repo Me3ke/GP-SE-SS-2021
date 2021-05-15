e73145ba35a1f8280d51c9f9c9da2e1739776c53
Onur Toraman <otoraman@techfak.uni-bielefeld.de>
2021-05-15 22:10:54 +0200

    Deleted the textarea functionality (was not a relevant task for sprint2). Also refactored some variables and css fixes, refs #29913 @0.2h and refs #30185 @1.5h

--- src/main/vue/views/ImpressumPage.vue
+++ src/main/vue/views/ImpressumPage.vue
@@ -6,18 +6,14 @@
                     <b-row class="base-header-row">
                         <b-col>
                             <BaseHeading class="baseHeader" name="" ></BaseHeading>
-                            <!--<h2 class="baseHeader-h2">{{$t('AboutPage.name')}}</h2>-->
                         </b-col>
                     </b-row>
                     <b-col style="margin-left: 2em; padding-left: 2em">
                         <h2 class="baseHeader-h2">{{$t('AboutPage.name')}}</h2>
-
                     </b-col>
                 </b-container>
-
-
-                <b-container class="content-container" style="margin-top: 2em">
-                    <b-col style="margin-top: 2em; margin-left: 4em">
+                <b-container class="content-container" style="margin-top: 6em">
+                    <b-col style="margin-top: -5em; margin-left: 4em">
                         <b-row class="provider-row" style="">
                             <b-col v-for="(inf, c) in info" :key="c">
                                 <b-row style="margin-bottom: -.4em"> <h5 class="row-header">{{$t('AboutPage.provider')}}</h5></b-row>
@@ -26,7 +22,6 @@
                                 <b-row> {{inf.zip}} {{inf.city}} </b-row>
                             </b-col>
                         </b-row>
-
                         <b-row class="contact-row">
                             <b-col v-for="(con, a) in contact" :key="a">
                                 <b-row style="margin-bottom: -.4em"> <h5 class="row-header">{{$t('AboutPage.contact')}}</h5></b-row>
@@ -35,36 +30,17 @@
                                 <b-row> {{con.mail}} </b-row>
                                 <b-row> {{con.web}} </b-row>
                             </b-col>
-
                         </b-row>
-
                         <b-row class="redak-row">
-                            <b-col v-for="(red, i) in redInfos" :key="i">
+                            <b-col v-for="(imprintInfo, i) in imprintInfos" :key="i">
                                 <b-row style="margin-bottom: -.4em"><h5 class="row-header">{{ $t('AboutPage.editorial') }}</h5></b-row>
-                                <b-row v-if="$i18n.locale === 'de'"> <h6 class="row-header">{{red.resp}}</h6> </b-row>
-                                <b-row v-else> <h6 class="row-header">{{red.respEN}}</h6> </b-row>
-                                <b-row> {{red.surname}} {{red.lastname}} </b-row>
-                                <b-row> {{red.address}} </b-row>
-                                <b-row> {{red.zip}} {{red.city}} </b-row>
+                                <b-row v-if="$i18n.locale === 'de'"> <h6 class="row-header2">{{imprintInfo.respDE}}</h6> </b-row>
+                                <b-row v-else> <h6 class="row-header2">{{imprintInfo.respEN}}</h6> </b-row>
+                                <b-row style="margin-top: -.5em"> {{imprintInfo.surname}} {{imprintInfo.lastname}} </b-row>
+                                <b-row> {{imprintInfo.address}} </b-row>
+                                <b-row> {{imprintInfo.zip}} {{imprintInfo.city}} </b-row>
                             </b-col>
-
                         </b-row>
-
-                        <b-container class="text-input-class">
-                            <b-row style="margin-top: 1em">
-                                <label style="padding-right: .5em">
-                                    <input type="text" v-model="redInfos[0].resp"> <b-button>Enter</b-button>
-                                </label>
-                            </b-row>
-                            <b-row style="padding-bottom: 3em">
-                                <label>
-                                    <input type="text" v-model="redInfos[0].respEN"> <b-button>Enter</b-button>
-                                </label>
-
-
-                            </b-row>
-                        </b-container>
-
                     </b-col>
                 </b-container>
         </b-container>
@@ -96,11 +72,11 @@ export default {
                     web: "www.mustermann.de"
                 }
             ],
-            redInfos: [
+            imprintInfos: [
                 {
                     //Verantwortlich nach § 55 Abs.2
-                    resp: "",
-                    respEN: "",
+                    respDE: "Verantwortlich nach § 55 Abs.2",
+                    respEN: "TEST",
                     surname: "Moritz",
                     lastname: "Schreiberling",
                     address: "Musterstraße 2",
@@ -136,7 +112,7 @@ export default {
     padding-left: -2em;
 }
 .content-container {
-    margin-left: 0em;
+    margin-left: 0;
 }
 .baseHeader {
     margin-top: 0.5em;
@@ -157,11 +133,11 @@ export default {
         margin-left: 2em;
     }
     .content-container {
-        margin-left: -2.4em;
+        margin-left: -.6em;
     }
     .baseHeader-h2 {
         margin-top: -1em !important;
-        margin-left: -31em !important;
+        margin-left: -30.3em !important;
     }
 
 }
@@ -171,10 +147,11 @@ export default {
         margin-left: -6em;
     }
     .content-container {
-        margin-left: -8em;
+        margin-left: -5.8em;
     }
     .baseHeader-h2 {
-        margin-left: -42em;
+        margin-left: -30em;
+        margin-top: -.9em;
     }
 
 }
@@ -183,40 +160,41 @@ export default {
 
 @media screen and (min-width: 992px) and (max-width: 1190px) {
     .full-container {
-        margin-left: 4em;
+        margin-left: 0;
     }
     .baseHeader-h2 {
         margin-left: -23.5em;
-        margin-top: -1em;
+        margin-top: -.8em;
     }
     .content-container {
-        margin-left: -4em;
+        margin-left: .6em;
     }
 }
 
 @media screen and (min-width: 801px) and (max-width: 991px) {
     .full-container {
-        margin-left: 4em;
+        margin-left: 1em;
     }
     .baseHeader-h2 {
-        margin-left: -12em;
+        margin-left: -17em;
+        margin-top: -.7em;
+    }
+    .content-container {
+        margin-left: 1em;
     }
 }
 
 
-
-
-
-@media screen and (min-width: 768px) {
+@media screen and (min-width: 768px) and (max-width: 800px) {
     .base-header-row {
-        margin-left: -0.8em !important;
+        margin-left: -1.8em;
         font-size: 1.3em;
     }
     .content-container {
         margin-left: -1.8em;
     }
     .baseHeader-h2 {
-        margin-left: -19em;
+        margin-left: -20em;
         margin-top: -0.8em;
     }
     .full-container {
@@ -243,31 +221,33 @@ export default {
 
 @media screen and (max-width: 500px) and (min-width: 460px) {
     .base-header-row {
-        font-size: 1.3em;
-        margin-left: -.6em;
+        font-size: 1em;
+        margin-left: -3em;
     }
     .content-container {
-        margin-left: -4em;
+        margin-left: -3em;
     }
     .baseHeader-h2 {
-        margin-left: -13em;
-        margin-top: -0.7em;
+        margin-left: -17em;
+        margin-top: -0.9em;
+        font-size: 1.6em;
     }
     .full-container {
         margin-left: 3em;
     }
+
 }
 
-@media screen and (max-width: 459px) {
+@media screen and (min-width: 459px) and (max-width: 499px) {
     .baseHeader-h2 {
         margin-top: -0.9em;
         font-size: 1.6em;
-        margin-left: -10.5em;
+        margin-left: -12.5em;
     }
 
     .baseHeader {
         margin-left: -2.8em;
-        margin-top: 0em;
+        margin-top: 0;
     }
     .full-container {
         margin-left: -1em;
@@ -275,20 +255,41 @@ export default {
     .content-container {
         padding-right: 2em;
     }
-    .text-input-class {
-        margin-left: -2em;
-    }
-
     .head-with-arrow {
         margin-left: 1em;
     }
 
 }
-
-@media screen and (max-width: 377px) {
+@media screen and (min-width: 410px) and (max-width: 458px) {
     .baseHeader-h2 {
         font-size: 1.5em;
-        margin-left: -8em;
+        margin-left: -13em;
+        margin-top: -.8em;
+    }
+    .redak-row {
+        font-size: .7em;
+    }
+    .base-header-row {
+        margin-left: 0.5em;
+    }
+    .contact-row {
+        font-size: .7em;
+    }
+    .provider-row {
+        font-size: .7em;
+    }
+    .row-header {
+        font-size: 1.3em;
+    }
+    .row-header2 {
+        font-size: .8em;
+    }
+}
+
+@media screen and (max-width: 409px) and (min-width: 355px) {
+    .baseHeader-h2 {
+        font-size: 1.5em;
+        margin-left: -11em;
         margin-top: -.8em;
     }
     .redak-row {
@@ -303,6 +304,75 @@ export default {
     .row-header {
         font-size: 1.3em;
     }
+    .row-header2 {
+        font-size: .8em;
+    }
 }
 
+@media screen and (max-width: 354px) and (min-width: 330px){
+    .baseHeader-h2 {
+        font-size: 1.5em;
+        margin-left: -10em;
+        margin-top: -.8em;
+    }
+    .redak-row {
+        font-size: .7em;
+    }
+    .contact-row {
+        font-size: .7em;
+    }
+    .provider-row {
+        font-size: .7em;
+    }
+    .row-header {
+        font-size: 1.3em;
+    }
+    .row-header2 {
+        font-size: .8em;
+    }
+}
+
+@media screen and (max-width: 329px) {
+    .baseHeader-h2 {
+        font-size: 1.5em;
+        margin-left: -9em;
+        margin-top: -.8em;
+    }
+    .redak-row {
+        font-size: .7em;
+    }
+    .contact-row {
+        font-size: .7em;
+    }
+    .provider-row {
+        font-size: .7em;
+    }
+    .row-header {
+        font-size: 1.3em;
+    }
+    .row-header2 {
+        font-size: .8em;
+    }
+    .content-container {
+        margin-left: -1em;
+    }
+}
+
+@media screen and (max-width: 304px) {
+    .redak-row {
+        font-size: .8em;
+        max-width: 80%;
+    }
+    .contact-row {
+        font-size: .8em;
+    }
+    .provider-row {
+        font-size: .8em;
+    }
+    .baseHeader-h2 {
+        margin-left: -7em;
+        margin-top: -.7em;
+    }
+
+}
 </style>
