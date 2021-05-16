9f691c41a671ce714dbdca8f4806301e6d2d723c
aheide <aheide@techfak.de>
2021-05-16 17:43:56 +0200

    I finished the inital design for changing/creating a keypair via a sweetalert dialogue; refs #29878 @4:00h

--- src/main/vue/components/UserInfoBox.vue
+++ src/main/vue/components/UserInfoBox.vue
@@ -36,7 +36,18 @@
           {{$t('UserInfoBox.publicKeys')}}
         </div>
         <b-list-group flush>
-          <b-list-group-item style="text-align: left">{{$t('UserInfoBox.publicKey')}} {{this.userInformation.publicKey}}</b-list-group-item>
+          <b-list-group-item>
+            <b-row>
+              <b-col style="text-align: left">
+                {{$t('UserInfoBox.publicKey')}} {{this.userInformation.publicKey}}
+              </b-col>
+              <b-col style="text-align: right">
+                <b-button @click="showAlert">
+                  {{$t('UserInfoBox.newKeypair')}}
+                </b-button>
+              </b-col>
+            </b-row>
+          </b-list-group-item>
         </b-list-group>
       </div>
     </b-row>
@@ -44,10 +55,76 @@
 </template>
 
 <script>
+
 export default {
   name: "UserInfoBox",
   props: {
     userInformation: Object,
+  },
+  methods: {
+    async showAlert() {
+      const inputOptions = new Promise((resolve) => {
+          resolve({
+            'generate': 'Generate keypair',
+            'upload': 'Upload public key',
+          })
+      })
+
+      const {value: updateOption} = await this.$swal.fire({
+        title: 'Select option',
+        input: 'radio',
+        width: 500,
+        inputOptions: inputOptions,
+        showCancelButton: true,
+        inputValidator: (value) => {
+          if (!value) {
+            return 'You need to choose something!'
+          }
+        }
+      })
+
+      if (updateOption === 'generate') {
+        this.$swal.fire({
+          title:'Warning',
+          icon: 'warning',
+          text: 'If you continue, your current keypair will be lost ' +
+          'forever. Are you sure you want to continue?',
+          confirmButtonText: `Continue`,
+          showCancelButton: true,
+          cancelButtonText: 'Cancel'
+        }).then((result) => {
+
+          if (result.isConfirmed) {
+            this.handleGenerateKeyPairs()
+            this.$swal.fire('Generating...')
+          }
+        })
+      }
+
+      else if (updateOption === 'upload') {
+        this.$swal.fire({
+          title:'Warning',
+          icon: 'warning',
+          text: 'If you continue, your current keypair will be lost ' +
+              'forever. Are you sure you want to continue?',
+          confirmButtonText: `Continue`,
+          showCancelButton: true,
+          cancelButtonText: 'Cancel'
+        }).then((result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) {
+            this.handleUploadKeyPairs
+            this.$swal.fire('Uploading...')
+          }
+        })
+      }
+    },
+    handleGenerateKeyPairs() {
+      //TODO Handling of the keypairs via generating
+    },
+    handleUploadKeyPairs() {
+      //TODO Handling of the keypairs via uploading
+    }
   }
 }
 </script>
@@ -55,7 +132,7 @@ export default {
 <style scoped>
 
 .card{
-  width: 40vw;
+  width: 50vw;
   margin-bottom: 5vh;
 }
 </style>
