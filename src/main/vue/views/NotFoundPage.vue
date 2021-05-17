1fd4446ac237695fc18192636c286a3c5b510cda
Hannah Schweizer <hschweizer@techfak.uni-bielefeld.de>
2021-05-17 10:08:08 +0200

    fixes #29790 @0.5h : made font size of 404 and no-connection pages more responsive

--- src/main/vue/views/NotFoundPage.vue
+++ src/main/vue/views/NotFoundPage.vue
@@ -1,43 +1,39 @@
 <template>
-    <b-container fluid id="container">
-        <b-row cols="1">
-            <b-col>
-                <Header></Header>
-            </b-col>
-        </b-row>
+    <div id="my-container">
 
-        <b-row class="mb-2 mt-5 text-center" align-v="center" align-h="center" cols="1">
-            <b-col id="h1" class="text">
-                <h1>{{ $t('NotFoundPage.h1') }}</h1>
-            </b-col>
-            <b-col class="text">
-                <h3>{{ $t('NotFoundPage.h3') }}</h3>
-            </b-col>
-            <b-col class="text">
-                <h6>{{ $t('NotFoundPage.errorCode') }}</h6>
-            </b-col>
-        </b-row>
+        <Header></Header>
 
-        <b-row class="mt-5 text-center" align-v="center" align-h="center" cols="2">
-            <b-col cols="3" class="text">
-                <div class="link" @click="historyThere()  ? $router.go(-1) : $router.push('/')">
-                    {{ $t('NotFoundPage.back') }}
-                </div>
-            </b-col>
-            <b-col cols="3" class="text">
-                <!-- To-Do: Add  real Route to Home -->
-                <div class="link" @click="$router.push(`/`)">
-                    {{ $t('NotFoundPage.start') }}
-                </div>
-            </b-col>
-        </b-row>
+        <b-container fluid>
+            <b-row class="mb-2 mt-5 text-center" align-v="center" align-h="center" cols="1">
+                <b-col>
+                    <h1>{{ $t('NotFoundPage.h1') }}</h1>
+                </b-col>
+                <b-col>
+                    <h3>{{ $t('NotFoundPage.h3') }}</h3>
+                </b-col>
+                <b-col>
+                    <h6>{{ $t('NotFoundPage.errorCode') }}</h6>
+                </b-col>
+            </b-row>
 
-        <b-row cols="1">
-            <b-col>
-                <Footer></Footer>
-            </b-col>
-        </b-row>
-    </b-container>
+            <b-row class="mt-3 text-center" align-v="center" align-h="center" cols="2">
+                <b-col cols="3" class="text">
+                    <div class="link" @click="historyThere()  ? $router.go(-1) : $router.push('/')">
+                        {{ $t('NotFoundPage.back') }}
+                    </div>
+                </b-col>
+                <b-col cols="3" class="text">
+                    <!-- To-Do: Add  real Route to Home -->
+                    <div class="link" @click="$router.push(`/`)">
+                        {{ $t('NotFoundPage.start') }}
+                    </div>
+                </b-col>
+            </b-row>
+        </b-container>
+
+        <Footer></Footer>
+
+    </div>
 </template>
 
 <script>
@@ -56,17 +52,65 @@ export default {
 </script>
 
 <style scoped>
-.text {
-    margin-bottom: 1vw;
+#my-container {
+    padding: 0;
+    margin: 0;
 }
 
-#container {
-    padding-right: 0;
-    padding-left: 0;
+/* Settings for differently sized screens */
+@media (max-width: 575.98px) {
+    h1 {
+        font-size: 1.3em;
+    }
+
+    h3 {
+        font-size: 0.6em;
+    }
+
+    h6, .text {
+        font-size: 0.5em;
+    }
 }
 
-#h1 {
-    margin-top: 7vw;
+@media (min-width: 576px) and (max-width: 767.98px) {
+    h1 {
+        font-size: 1.5em;
+    }
+
+    h3 {
+        font-size: 0.8em;
+    }
+
+    h6, .text {
+        font-size: 0.7em;
+    }
 }
 
+@media (min-width: 768px) and (max-width: 991.98px) and (max-height: 499.98px) {
+    h1 {
+        font-size: 1.6em;
+    }
+
+    h3 {
+        font-size: 0.9em;
+    }
+
+    h6, .text {
+        font-size: 0.8em;
+    }
+}
+
+@media (min-width: 768px) and (max-width: 991.98px) and (min-height: 500px) {
+    h1 {
+        font-size: 1.6em;
+    }
+
+    h3 {
+        font-size: 0.9em;
+    }
+
+    h6, .text {
+        font-size: 0.8em;
+    }
+}
 </style>
