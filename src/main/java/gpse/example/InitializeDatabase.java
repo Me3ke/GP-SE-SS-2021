61c61ff592f1cdb7e27ff8c54d07317444593464
Jan Lukas Kronsbein <jkronsbein@techfak.uni-bielefeld.de>
2021-05-18 12:32:24 +0200

    Hotfix: Mvn errors

--- src/main/java/gpse/example/InitializeDatabase.java
+++ src/main/java/gpse/example/InitializeDatabase.java
@@ -30,13 +30,13 @@ public class InitializeDatabase implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() {
-        String username = "hans.schneider@mail.de";
+        final String username = "hans.schneider@mail.de";
         try {
             userService.loadUserByUsername(username);
         } catch (UsernameNotFoundException ex) {
             final PersonalData personalData = new PersonalData("Berliner Straße", 2, 12312,
                 "Liebefeld", "Deutschland", LocalDate.now(), 32131245);
-            userService.createUser(
+            final User user = userService.createUser(
                 username,
                 "{bcrypt}$2y$12$DdtBOd4cDqlvMGXPoNr9L.6YkszYXn364x172BKabx3ucOiYUmTfG",
                 "Hans",
@@ -44,6 +44,8 @@ public class InitializeDatabase implements InitializingBean {
                 personalDataService.savePersonalData(personalData),
                 "ROLE_USER"
             );
+            System.out.println("Created new User: " + user.getUsername());
+
 
         }
     }
