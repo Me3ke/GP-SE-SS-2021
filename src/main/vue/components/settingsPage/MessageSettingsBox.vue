db7d8a9ea2a9768a4ec34bfb1f1f344526d59269
Hannah Schweizer <hschweizer@techfak.uni-bielefeld.de>
2021-05-26 07:52:54 +0200

    refs #30308 @0.75h: added message settings to user/settings page, made user/settings page design consistent

--- src/main/vue/components/settingsPage/MessageSettingsBox.vue
+++ src/main/vue/components/settingsPage/MessageSettingsBox.vue
@@ -5,16 +5,16 @@
             <div class="card">
                 <div class="card-header" style="background-color: var(--elsa-blue); color: var(--whitesmoke);">
                     <h4>
-                        {{ $t('MessageSettings.settings') }}
+                        {{ $t('Settings.MessageSettings.settings') }}
                     </h4>
                 </div>
                 <b-list-group flush>
                     <b-list-group-item class="d-flex justify-content-between align-items-center">
                           <span>
-                             {{ $t('MessageSettings.toDo.text') }}
-                               <b-icon id="tooltip-target-1" icon="info-circle-fill" class="my-icon"></b-icon>
+                             {{ $t('Settings.MessageSettings.toDo.text') }}
+                               <b-icon id="tooltip-target-1" icon="info-circle" class="my-icon"></b-icon>
                               <b-tooltip target="tooltip-target-1" triggers="hover">
-                                   {{ $t('MessageSettings.toDo.exp') }}
+                                   {{ $t('Settings.MessageSettings.toDo.exp') }}
                               </b-tooltip>
                           </span>
                         <!-- TODO: Add badge-on/badge-off class depending on user preference, same for text-->
@@ -27,10 +27,10 @@
 
                     <b-list-group-item class="d-flex justify-content-between align-items-center">
                         <span>
-                              {{ $t('MessageSettings.sign.text') }}
-                             <b-icon id="tooltip-target-2" icon="info-circle-fill" class="my-icon"></b-icon>
+                              {{ $t('Settings.MessageSettings.sign.text') }}
+                             <b-icon id="tooltip-target-2" icon="info-circle" class="my-icon"></b-icon>
                               <b-tooltip target="tooltip-target-2" triggers="hover">
-                                   {{ $t('MessageSettings.sign.exp') }}
+                                   {{ $t('Settings.MessageSettings.sign.exp') }}
                               </b-tooltip>
                         </span>
                         <!-- TODO: Add badge-on/badge-off class depending on user preference, same for text-->
@@ -41,10 +41,10 @@
 
                     <b-list-group-item class="d-flex justify-content-between align-items-center">
                         <span>
-                                   {{ $t('MessageSettings.read.text') }}
-                             <b-icon id="tooltip-target-3" icon="info-circle-fill" class="my-icon"></b-icon>
+                                   {{ $t('Settings.MessageSettings.read.text') }}
+                             <b-icon id="tooltip-target-3" icon="info-circle" class="my-icon"></b-icon>
                               <b-tooltip target="tooltip-target-3" triggers="hover">
-                                   {{ $t('MessageSettings.read.exp') }}
+                                   {{ $t('Settings.MessageSettings.read.exp') }}
                               </b-tooltip>
                         </span>
                         <!-- TODO: Add badge-on/badge-off class depending on user preference, same for text-->
@@ -55,10 +55,10 @@
 
                     <b-list-group-item class="d-flex justify-content-between align-items-center">
                         <span>
-                             {{ $t('MessageSettings.progress.text') }}
-                             <b-icon id="tooltip-target-4" icon="info-circle-fill" class="my-icon"></b-icon>
+                             {{ $t('Settings.MessageSettings.progress.text') }}
+                             <b-icon id="tooltip-target-4" icon="info-circle" class="my-icon"></b-icon>
                               <b-tooltip target="tooltip-target-4" triggers="hover">
-                                   {{ $t('MessageSettings.progress.exp') }}
+                                   {{ $t('Settings.MessageSettings.progress.exp') }}
                               </b-tooltip>
                         </span>
                         <!-- TODO: Add badge-on/badge-off class depending on user preference, same for text-->
@@ -69,10 +69,10 @@
 
                     <b-list-group-item class="d-flex justify-content-between align-items-center">
                         <span>
-                            {{ $t('MessageSettings.newVersion.text') }}
-                             <b-icon id="tooltip-target-5" icon="info-circle-fill" class="my-icon"></b-icon>
+                            {{ $t('Settings.MessageSettings.newVersion.text') }}
+                             <b-icon id="tooltip-target-5" icon="info-circle" class="my-icon"></b-icon>
                               <b-tooltip target="tooltip-target-5" triggers="hover">
-                                   {{ $t('MessageSettings.newVersion.exp') }}
+                                   {{ $t('Settings.MessageSettings.newVersion.exp') }}
                               </b-tooltip>
                         </span>
                         <!-- TODO: Add badge-on/badge-off class depending on user preference, same for text-->
@@ -83,10 +83,10 @@
 
                     <b-list-group-item class="d-flex justify-content-between align-items-center">
                         <span>
-                             {{ $t('MessageSettings.protocol.text') }}
-                             <b-icon id="tooltip-target-6" icon="info-circle-fill" class="my-icon"></b-icon>
+                             {{ $t('Settings.MessageSettings.protocol.text') }}
+                             <b-icon id="tooltip-target-6" icon="info-circle" class="my-icon"></b-icon>
                               <b-tooltip target="tooltip-target-6" triggers="hover">
-                                   {{ $t('MessageSettings.protocol.exp') }}
+                                   {{ $t('Settings.MessageSettings.protocol.exp') }}
                               </b-tooltip>
                         </span>
                         <!-- TODO: Add badge-on/badge-off class depending on user preference, same for text-->
@@ -112,115 +112,5 @@ export default {
 }
 </script>
 
-<style scoped>
-
-.my-icon {
-    fill: var(--elsa-blue);
-    height: 1em;
-    width: auto;
-}
-
-/* Badges aka On/off switches */
-.badge-on {
-    background-color: var(--elsa-blue);
-    color: var(--whitesmoke);
-}
-
-.badge-off {
-    background-color: var(--elsa-blue-transparent);
-    color: var(--shadow-grey);
-}
-
-.badge-on:hover, .badge-off:hover {
-    background-color: var(--open-doc-hover);
-    transition: 0.4s;
-}
-
-.badge-off:hover {
-    color: var(--whitesmoke);
-}
-
-.list-group-item {
-    background-color: var(--user-info)
-}
-
-.card {
-    width: 100%;
-    margin-bottom: 5vh;
-    background-color: var(--whitesmoke);
-    border-color: var(--dark-grey);
-}
-
-/* Extra small devices (portrait phones, less than 576px) */
-@media (max-width: 575.98px) {
-    h4 {
-        font-size: .92em;
-        text-overflow-mode: ellipse;
-    }
-
-    span {
-        font-size: 0.6em;
-    }
-
-}
-
-/* Small devices (landscape phones, 576px and up) */
-@media (min-width: 576px) and (max-width: 767.98px) {
-    h4 {
-        font-size: .92em;
-        text-overflow-mode: ellipse;
-    }
-
-    span {
-        font-size: 0.7em;
-    }
-
-}
-
-/* Large Phones in landscape mode */
-@media (min-width: 768px) and (max-width: 991.98px) and (max-height: 499.98px) {
-    h4 {
-        font-size: .92em;
-        text-overflow-mode: ellipse;
-    }
-
-    span {
-        font-size: 0.7em;
-    }
-}
-
-/* Medium devices (tablets, 768px and up) */
-@media (min-width: 768px) and (max-width: 991.98px) and (min-height: 500px) {
-    h4 {
-        font-size: 1.25em;
-        text-overflow-mode: ellipse;
-    }
-
-    span {
-        font-size: 0.7em;
-    }
-
-}
-
-/* Large devices (desktops, 992px and up)*/
-@media (min-width: 992px) and (max-width: 1199.98px) {
-    h4 {
-        font-size: 1.2em;
-        text-overflow-mode: ellipse;
-    }
-
-    span {
-        font-size: 0.9em;
-    }
-}
-
-/* Extra large devices (large desktops, 1200px and up)*/
-@media (min-width: 1200px) {
-    h4 {
-        font-size: 1.3em;
-        text-overflow-mode: ellipse;
-    }
-}
-
-
+<style scoped src="../../assets/css/settingsPage.css">
 </style>
---
+++ src/main/vue/components/settingsPage/UserInfoBox.vue
@@ -0,0 +1,74 @@
+<template>
+    <b-container fluid="sm">
+
+        <b-row align-h="center" align-v="center" class="text-center">
+            <div class="card">
+                <div class="card-header" style="background-color: var(--elsa-blue); color: var(--whitesmoke);">
+                    <h4>
+                        {{ $t('Settings.UserInfoBox.accountInformation') }}
+                    </h4>
+                </div>
+
+                <b-list-group flush>
+                    <b-list-group-item class="d-flex justify-content-between align-items-center">
+                        <span>
+                            {{ $t('Settings.UserInfoBox.email') }} {{ this.user.email }}
+                        </span>
+                    </b-list-group-item>
+                    <b-list-group-item class="d-flex justify-content-between align-items-center">
+                       <span>
+                            {{ $t('Settings.UserInfoBox.firstName') }} {{ this.user.firstname }}
+                        </span></b-list-group-item>
+                    <b-list-group-item class="d-flex justify-content-between align-items-center">
+                       <span>
+                            {{ $t('Settings.UserInfoBox.lastName') }} {{ this.user.lastname }}
+                        </span>
+                    </b-list-group-item>
+                    <b-list-group-item class="d-flex justify-content-between align-items-center">
+                       <span>
+                            {{ $t('Settings.UserInfoBox.birthday') }} {{ this.userData.birthday }}
+                        </span>
+                    </b-list-group-item>
+                    <b-list-group-item class="d-flex justify-content-between align-items-center">
+                       <span>
+                            {{ $t('Settings.UserInfoBox.phoneNumber') }} {{ this.userData.phoneNumber }}
+                        </span>
+                    </b-list-group-item>
+                    <b-list-group-item class="d-flex justify-content-between align-items-center">
+                       <span>
+                            {{ $t('Settings.UserInfoBox.country') }} {{ this.userData.country }}
+                        </span>
+                    </b-list-group-item>
+                    <b-list-group-item class="d-flex justify-content-between align-items-center">
+                       <span>
+                            {{ $t('Settings.UserInfoBox.city') }} {{ this.userData.homeTown }}
+                        </span>
+                    </b-list-group-item>
+                    <b-list-group-item class="d-flex justify-content-between align-items-center">
+                       <span>
+                            {{ $t('Settings.UserInfoBox.street') }} {{ this.userData.street }}
+                        </span>
+                    </b-list-group-item>
+                    <b-list-group-item class="d-flex justify-content-between align-items-center">
+                       <span>
+                            {{ $t('Settings.UserInfoBox.houseNumber') }} {{ this.userData.houseNumber }}
+                        </span>
+                    </b-list-group-item>
+                </b-list-group>
+            </div>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+    name: "UserInfoBox",
+    props: {
+        user: Object,
+        userData: Object
+    }
+}
+</script>
+
+<style scoped src="../../assets/css/settingsPage.css">
+</style>
