91cb2ed72e9e172e30fb8cb460faf683a7e7e10d
Onur Toraman <otoraman@techfak.uni-bielefeld.de>
2021-06-03 22:38:01 +0200

    Added an example usage for the fileToBase64Converter.js

--- src/main/vue/components/fileToBase64Converter.js
+++ src/main/vue/components/fileToBase64Converter.js
@@ -1,5 +1,16 @@
 
 
+/* example Usage:
+
+       async asyncHandleFunction() {
+          return await convertUploadFileToBase64(file)     <- your selected file which is going to be uploaded
+        },
+
+        and this you only have to save into the string e.g:
+            documentObject.data = await asyncHandleFunction
+
+ */
+
 // converting the selected file into base64 (as promise)
 export const convertUploadFileToBase64 = (file) => {
     const reader = new FileReader()
