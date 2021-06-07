f12c45229f7a451300df6e82b594ce1690972c5d
Jonas <jonashaschke@icloud.com>
2021-06-07 14:37:20 +0200

    added ScheduledTask class and base logic for sending the reminder mail. #30841 @0.75h

---
+++ src/main/java/gpse/example/util/ScheduledTasks.java
@@ -0,0 +1,50 @@
+package gpse.example.util;
+
+import gpse.example.domain.documents.Document;
+import gpse.example.domain.documents.DocumentService;
+import gpse.example.domain.signature.Signatory;
+import gpse.example.util.email.SMTPServerHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class ScheduledTasks {
+
+    @Autowired
+    DocumentService documentService;
+
+    @Autowired
+    SMTPServerHelper smtpServerHelper;
+
+    private static final int MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
+
+    @Scheduled(fixedRate = MILLISECONDS_PER_DAY)
+    public void checkForOpenReminder() {
+        for (Document doc: documentService.getDocuments()) {
+            if(doc.isOrderRelevant()) {
+                informSignatoriesInOrder(doc);
+            } else {
+                informSignatoriesWithoutOrder(doc);
+            }
+        }
+    }
+
+    public void informSignatoriesInOrder(Document doc) {
+
+    }
+
+    public void informSignatoriesWithoutOrder(Document doc) {
+        for (Signatory signatory:doc.getSignatories()) {
+            if (signatory.getReminder() > -1) {
+                if (LocalDateTime.now().isAfter(doc.getEndDate().minusDays(signatory.getReminder()))){
+
+                }
+            }
+        }
+
+    }
+
+}
