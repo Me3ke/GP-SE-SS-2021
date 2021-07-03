848fae667d6aedba6710c974456f08df359ced60
Onur Toraman <otoraman@techfak.uni-bielefeld.de>
2021-07-03 12:22:01 +0200

    Created the component for the impressum editor, refs #31223 @0.4h

---
+++ src/main/vue/components/ImpressumEditor.vue
@@ -0,0 +1,56 @@
+<template>
+    <b-container style="margin-top: 5em">
+        <quill-editor
+        ref="editor"
+        class="editor"
+        v-model="text"
+        ></quill-editor>
+
+
+        <button type="button"
+                class="mt-1 light-btn"
+                v-b-modal="'modal-preview'"
+        >
+                    <span class="button-txt">
+                        Preview
+                    </span>
+        </button>
+        <!--- Preview --->
+        <b-modal
+            :id="'modal-preview'"
+            centered
+            :title="'Email Template Preview'"
+            hide-footer ok-only
+            v-if="text"
+            style="margin-top: 2em"
+        >
+            <div v-html="text"></div>
+        </b-modal>
+    </b-container>
+</template>
+
+<script>
+export default {
+    name: "ImpressumEditor",
+
+    data() {
+        return {
+            text: '<h2><strong>Impressum</strong></h2>' +
+                '<p>Anbieter:<br />Max Mustermann<br />Musterstraße 1 \n<br />80999 München</p>' +
+                '<p>Kontakt:<br />Telefon: 089/1234567-8<br />Telefax: 089/1234567-9<br />E-Mail: mail@mustermann.de<br />Website: www.mustermann.de</p>' +
+                '<p> </p>' +
+                '<p>Bei redaktionellen Inhalten:</p>' +
+                '<p>Verantwortlich nach § 55 Abs.2 RStV<br />Moritz Schreiberling<br />Musterstraße 2<br />80999 München</p>'
+        }
+    },
+    methods: {
+        test() {
+            console.log(this.text)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
