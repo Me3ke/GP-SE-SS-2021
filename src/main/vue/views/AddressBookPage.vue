aacf56881bd6e0c65954ca6fee2ff81432d139ef
Hannah Schweizer <hschweizer@techfak.uni-bielefeld.de>
2021-07-09 09:27:45 +0200

    refs #31572 @0.08h: added blank AddressBookPage Component, added into Avatar.vue possibility to go to address book

---
+++ src/main/vue/views/AddressBookPage.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <Header></Header>
+
+    <BaseHeading name="AddressBook.title"></BaseHeading>
+
+    <Footer></Footer>
+</div>
+</template>
+
+<script>
+import Header from "@/main/vue/components/header/Header";
+import Footer from "@/main/vue/components/Footer";
+
+export default {
+    name: "AddressBookPage",
+    components:{
+        Footer,
+        Header
+    }
+}
+</script>
+
+<style scoped src="../assets/css/userManagement.css">
+</style>
