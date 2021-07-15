a6b2d2b1d6ab7d8a3aea349ee5c751b2379ed8bf
Onur Toraman <otoraman@techfak.uni-bielefeld.de>
2021-07-15 16:35:59 +0200

    Added an error upload page and removed unused data variable, refs #31211

--- src/main/vue/components/uploadNewVersionButton.vue
+++ src/main/vue/components/uploadNewVersionButton.vue
@@ -1,271 +1,5 @@
 <template>
     <div>
-        <!-- Page 1 -->
-        <!---
-        <b-modal
-            modal-class="model-class"
-            :id="'modal-' + docID + 'a'"
-            ref="modal-page1"
-            centered :title="$t('UploadDoc.UpdateDocument.replaceDoc') + document.title"
-            style="background-color: var(--whitesmoke); color: var(--dark-grey)"
-            hide-footer ok-only no-stacking
-        >
-            <div>
-                <h6>{{ $t('UploadDoc.UpdateDocument.selectDocument') }}</h6>
-            </div>
-            <div>
-                <div class="modal-body">
-                    <div>
-                        <div class="form-group files">
-                            <b-form-file
-                                v-model="file"
-                                :state="Boolean(file)"
-                                id="fileInput"
-                                v-bind:placeholder="$t('UploadDoc.UpdateDocument.chooseFile')"
-                                drop-placeholder="Drop file here..."
-                                @change="previewFile"
-                            ></b-form-file>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="text-right">
-                <button type="button" class="mt-1 light-btn" @click="resetSettings('modal-page1')">
-                    <span class="button-txt">
-                        {{ $t('UploadDoc.close') }}
-                    </span>
-                </button>
-
-                <button type="button"
-                        class="mt-1 elsa-blue-btn"
-                        :disabled="file == null"
-                        @click="setActualDoc"
-                >
-                    <span class="button-txt">
-                        {{ $t('UploadDoc.continue') }}
-                    </span>
-                </button>
-            </div>
-        </b-modal>
-
-        <--- Skip or Add Readers ---
-        <b-modal
-            modal-class="model-class"
-            :id="'modal-' + docID + 'bb'"
-            ref="modal-page2"
-            centered
-            :title="document.title + '  Documentsettings'"
-            hide-footer ok-only no-stacking
-        >
-            <div>
-                <button type="button"
-                        class="light-btn"
-                        @click="review = true;"
-                        v-b-modal="'modal-' + docID + 'bbb'"
-                >
-                    <h5>
-                        {{ $t('UploadDoc.addReaders') }}
-                    </h5>
-                </button>
-                <button type="button"
-                        class="light-btn"
-                        @click="review = false;"
-                        v-b-modal="'modal-' + docID + 'bbbb'"
-
-                >
-                    <h5>
-                        {{ $t('UploadDoc.skipReview') }}
-                    </h5>
-                </button>
-            </div>
-        </b-modal>
-
-            Readers
-        <b-modal
-            style="margin-top: 2em;"
-            modal-class="model-class"
-            :id="'modal-' + docID + 'bbb'"
-            ref="modal-page2-reader"
-            centered
-            :title="document.title"
-            hide-footer ok-only no-stacking
-        >
-            <div>
-                <ReaderMenu :readers="actualDoc.readers"></ReaderMenu>
-            </div>
-
-            <div>
-                <button type="button"
-                        class="mt-1 light-btn"
-                        v-b-modal="'modal-' + docID + 'b'"
-                        @click="showHideModal('modal-page2-reader', 'modal-page2')"
-
-                >
-                    <span class="button-txt">
-                        {{ $t('UploadDoc.back') }}
-                    </span>
-                </button>
-
-                <button type="button"
-                        class="ml-1 elsa-blue-btn"
-                        v-b-modal="'modal-' + docID + 'bbbb'"
-                >
-                    <span class="button-txt">
-                        {{ $t('UploadDoc.continue') }}
-                    </span>
-                </button>
-            </div>
-
-        </b-modal>
-
-
-        <---EndDate + Signatories---
-        <b-modal
-            modal-class="model-class"
-            :id="'modal-' + docID + 'bbbb'"
-            ref="modal-page2-signatories"
-            centered
-            :title="document.title"
-            style="margin-top: 2em; padding-top: 2em"
-            hide-footer ok-only no-stacking
-        >
-            <div>
-                <div>
-                    <label for="endDatePicker">{{ $t('Settings.DocumentSettings.chooseDate') }}</label>
-                    <b-form-datepicker id="endDatePicker" v-model="actualDoc.endDate" class="mb-2"></b-form-datepicker>
-                </div>
-                <SignatoryMenu :signatories="actualDoc.signatories" :orderRelevant="actualDoc.orderRelevant"
-                               @update-signatories="updateSignatories"></SignatoryMenu>
-            </div>
-
-            <div class="text-right">
-                <button type="button"
-                        class="mt-1 light-btn"
-                        v-b-modal="this.actualDoc.review ? 'modal-' + docID + 'bbb' : 'modal-' + docID + 'bb' "
-                        @click="showHideModal('modal-page2-signatories',  'modal-page2' )">
-
-                    <span class="button-txt">
-                        {{ $t('UploadDoc.back') }}
-                    </span>
-                </button>
-
-                <button type="button"
-                        class="ml-1 elsa-blue-btn"
-                        v-b-modal="'modal-' + docID + 'c'"
-                >
-                    <span class="button-txt">
-                        {{ $t('UploadDoc.continue') }}
-                    </span>
-                </button>
-            </div>
-
-        </b-modal>
-
-        <--- Email Template --
-        <b-modal
-            modal-class="model-class"
-            :id="'modal-'+ docID + 'c'"
-            ref="modal-page3"
-            class="modal-emailTemplate"
-            centered scrollable
-            :title= "document.title + '  ' +
-            $t('EmailTemplate.emailTemp') "
-            style="margin-top: 2em"
-            ok-only hide-footer
-        >
-            <b-container>
-
-                <b-row style="padding-bottom: 1em">
-                    <---emit email templates with '@saveEmailTemplate' ---
-                    <EmailTemplate @saveEmailTemplate="setEmailTemplate"></EmailTemplate>
-                </b-row>
-            </b-container>
-            <div style="padding-bottom: 1em; text-align: right" class="flex-box-2">
-
-                <b-button
-                    class="mt-1 light-btn"
-                    @click="showHideModal('modal-page3' , 'modal-page2-signatories')"
-                    style="margin-top: 0.2em; margin-bottom: 0.1em; margin-left: 0.7em; width: auto"
-                >
-                    {{ $t('UploadDoc.back') }}
-                </b-button>
-
-                <b-button
-                      class="ml-1 elsa-blue-btn"
-                      @click="showHideModal('modal-page3', 'modal-page4')"
-                      style="margin-top: 0.2em; margin-bottom: 0.1em; margin-left: 0.7em; width: auto;"
-                >
-                    {{ $t('UploadDoc.continue') }}
-                </b-button>
-            </div>
-
-        </b-modal>
-        <-- Modal Page for save written email template---
-        <-- Last PAGE  --
-        <-- Last PAGE  --
-        <b-modal
-            modal-class="model-class"
-            :id="'modal-' + docID + 'd'"
-            ref="modal-page4"
-            centered
-            hide-footer hide-header ok-only no-stacking
-        >
-            <div>
-                <h3>{{ document.title }}</h3>
-                <h5> {{ $t('UploadDoc.UpdateDocument.confirmation', {documentTitle: document.title}) }}</h5>
-            </div>
-            <p class="my-4"> {{ $t('UploadDoc.UpdateDocument.signatureResetReminder') }} {{ showAlert }} </p>
-            <div class="text-right">
-
-
-                <button type="button"
-                        class="mt-1 light-btn"
-                        v-b-modal="'modal-' + docID + 'c'"
-                        @click="showHideModal('modal-page4', 'modal-page3')">
-
-                    <span class="button-txt">
-                        {{ $t('UploadDoc.back') }}
-                    </span>
-                </button>
-
-                <button type="button"
-                        class="ml-1 elsa-blue-btn"
-                        @click="uploadNewFile"
-                >
-                    <span class="button-txt">
-                        {{ $t('UploadDoc.upload') }} <--UPLOAD --
-                    </span>
-                </button>
-
-
-
-                <--<b-button
-                    @click="uploadNewFile"
-                    >
-                    {{ $t('UploadDoc.UpdateDocument.update1') }}
-                </b-button>--
-            </div>
-        </b-modal>
-
-
-        <-- TODO Error Page  --
-        <b-modal
-            modal-class="model-class"
-            :id="'modal-' + docID + 'error'"
-            centered
-            hide-footer hide-header ok-only no-stacking
-
-        >
-            <b-alert align="center">
-                ERROR Page
-            </b-alert>
-
-        </b-modal>
-
-        --->
-
-
         <div v-if="clicked && !uploadingDocument">
             <transition>
                 <div class="modal-mask">
@@ -273,13 +7,20 @@
                         <div class="modal-dialog h-100 d-flex flex-column justify-content-center my-0">
                             <div class="modal-content">
                                 <div class="modal-header" style="justify-content: space-between">
-                                    <h5 v-if="page === 1" class="modal-title">{{ $t('UploadDoc.UpdateDocument.update2') }}</h5>
-                                    <h5 v-if="page === 2" class="modal-title">{{ $t('UploadDoc.UpdateDocument.update2') }}</h5>
-                                    <h5 v-if="page === 3" class="modal-title">{{ $t('UploadDoc.UpdateDocument.update2') }}</h5>
-                                    <h5 v-if="page === 4" class="modal-title">{{ $t('UploadDoc.UpdateDocument.update2') }}</h5>
-                                    <h5 v-if="page === 5" class="modal-title">{{ $t('UploadDoc.UpdateDocument.update2') }}</h5>
-                                    <h5 v-if="page === 6" class="modal-title">{{ $t('UploadDoc.UpdateDocument.update2') }}</h5>
-
+                                    <h5 v-if="page === 1" class="modal-title">
+                                        {{$t('UploadDoc.UpdateDocument.update2')}}</h5>
+                                    <h5 v-if="page === 2" class="modal-title">
+                                        {{$t('UploadDoc.UpdateDocument.update2') }}</h5>
+                                    <h5 v-if="page === 3" class="modal-title">
+                                        {{$t('UploadDoc.UpdateDocument.update2')}}</h5>
+                                    <h5 v-if="page === 4" class="modal-title">
+                                        {{$t('UploadDoc.UpdateDocument.update2')}}</h5>
+                                    <h5 v-if="page === 5" class="modal-title">
+                                        {{$t('UploadDoc.UpdateDocument.update2')}}</h5>
+                                    <h5 v-if="page === 6" class="modal-title">
+                                        {{$t('UploadDoc.UpdateDocument.update2')}}</h5>
+                                    <h5 v-if="page === 7" class="modal-title">
+                                        {{$t('UploadDoc.UpdateDocument.errorUpload')}}</h5>
 
                                     <h5>
                                         <b-icon type="button" icon="x-square" @click="show = false; close()">
@@ -310,17 +51,17 @@
                                                     <b-col cols="auto">
                                                         <button type="button" class="light-btn"
                                                                 @click="page = page - 1">
-                                                            <span>
-                                                                {{ $t('UploadDoc.back') }}
-                                                            </span>
+                                                                <span>
+                                                                    {{ $t('UploadDoc.back') }}
+                                                                </span>
                                                         </button>
                                                     </b-col>
                                                     <b-col cols="auto">
                                                         <button type="button" class="elsa-blue-btn"
                                                                 @click="page = page + 1">
-                                                            <span>
-                                                                {{ $t('UploadDoc.continue') }}
-                                                            </span>
+                                                                <span>
+                                                                    {{ $t('UploadDoc.continue') }}
+                                                                </span>
                                                         </button>
                                                     </b-col>
                                                 </b-row>
@@ -336,9 +77,6 @@
                                                     @showEmailTemplate="showEmailTemplate"
                                                     @previousPage="page = page - 1"></UploadSettings>
                                 </div>
-
-
-
 
 
                                 <!-- Page 4: Email Template (for new User) -->
@@ -395,15 +133,14 @@
                                 </div>
 
 
-
-
                                 <!-- Page 6: Upload -->
                                 <div v-if="page === 6">
                                     <div>
                                         <div class="modal-body">
                                             <div v-if="!this.uploadingDocument">
                                                 <div>
-                                                    <p> {{$t('UploadDoc.UpdateDocument.confirmation', {documentTitle: document.title})}}</p>
+                                                    <p>
+                                                        {{ $t('UploadDoc.UpdateDocument.confirmation', {documentTitle: document.title}) }}</p>
                                                     <p>{{ $t('UploadDoc.UpdateDocument.signatureResetReminder') }}</p>
                                                 </div>
                                             </div>
@@ -432,25 +169,21 @@
                                 </div>
 
 
-
                                 <!-- Page 7 Error -->
                                 <div v-if="page === 7">
-                                    <div v-if="showAlert">
+                                    <div>
                                         <div class="modal-body">
                                             <div>
                                                 <div>
-                                                    Fehler! Bitte versuchen Sie es später noch einmal
+                                                    {{ $t('UploadDoc.UpdateDocument.errorWhileUpload') }}
                                                 </div>
                                             </div>
-
-
-
                                         </div>
                                         <div class="modal-footer">
                                             <b-row align-h="end">
                                                 <b-col cols="auto">
                                                     <button class="light-btn" @click="close">
-                                                        {{ $t('UploadDoc.back') }}
+                                                        {{ $t('UploadDoc.close') }}
                                                     </button>
                                                 </b-col>
                                             </b-row>
@@ -465,8 +198,6 @@
                 </div>
             </transition>
         </div>
-
-
     </div>
 </template>
 
@@ -482,11 +213,8 @@ export default {
     data() {
         return {
 
-
             show: true,
-
             showTemplateBoolean: false,
-
             showFooter: true,
             page: 1,
             selectedEnvelope: {
@@ -499,13 +227,6 @@ export default {
                 orderRelevant: true
                 // TODO: processStart: Boolean
             },
-            settings2: {
-                signatories: [],
-                endDate: null,
-                orderRelevant: true,
-                readers: []
-                // TODO: processStart: Boolean
-            },
             file: {
                 data: null,
                 type: null,
@@ -516,16 +237,7 @@ export default {
 
             // filename of uploaded file
             fileString: "",
-            file2: null,
-            showAlert: false,
-            review: true,
-
-
-            // need document settings as variable
-            newTitle: '',
-
-
-            // TODO Actual Doc which is going to be replaced
+            // Object which contains the selected document and getting updated with the new informations
             actualDoc: {},
         }
     },
@@ -583,7 +295,7 @@ export default {
 
             // here show error
 
-            if(this.newDocumentError === undefined) {
+            if (this.newDocumentError === undefined) {
 
 
                 await this.$store.dispatch('document/fetchDocumentInfo', {envId: this.envID, docId: this.newDocumentId})
@@ -598,7 +310,7 @@ export default {
 
                 this.close()
             } else {
-                this.showAlert = true
+                this.page = 7
             }
         },
 
